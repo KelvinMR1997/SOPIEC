c5ce06581200493bad1011c2b3b00efcdc8ca572
KelvinMR1997 <57110967+KelvinMR1997@users.noreply.github.com>
2020-12-08 01:42:27 -0500

    db

--- assets/php/db.php
+++ assets/php/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$DB = 'sopiecdb';
+$host = 'bow2b2l0x3hxxcrkvqpo-mysql.services.clever-cloud.com';
+$user = 'un2z6tkjgfvvwwqc';
+$password = 'nsrOtYaYFeHGw7aq9yUL';
+$DB = 'bow2b2l0x3hxxcrkvqpo';
 
 $conexion= mysqli_connect($host,$user,$password,$DB);
 
